4acbc61c0c7e22b6ca140e710efa2d74f6a2d949
LeticiaCarrias <leticiacarrias01@gmail.com>
2017-04-12 15:06:51 -0300

    Add files via upload

---
+++ evento.js
@@ -0,0 +1,9 @@
+function evento_entrar(){
+alert ("seja bem vindo");
+}
+function evento_sair(){
+alert("ate a proxima visita");
+}
+function evento_clique(){
+window.document.getElementById('id2').style.color='red';
+}
